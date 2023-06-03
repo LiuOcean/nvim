30287194f721126839c21b3e154be6f2287b8889
L <401419353@qq.com>
2023-06-03 16:54:08 +0800

    增加配置

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+require'nvim-treesitter.configs'.setup {
+  -- 添加不同语言
+  ensure_installed="all",
+  highlight = { enable = true },
+  indent = { enable = true },
+
+  -- 不同括号颜色区分
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+}
